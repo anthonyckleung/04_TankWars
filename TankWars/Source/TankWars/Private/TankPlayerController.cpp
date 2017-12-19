b478ee8a163f29d8348d009611be1f15772a126f
aleung124 <anthonyleung124@gmail.com>
2017-12-19 18:11:34 -0500

    Find screen Pixel location implemented in TankPlayerController.cpp

--- TankWars/Source/TankWars/Private/TankPlayerController.cpp
+++ TankWars/Source/TankWars/Private/TankPlayerController.cpp
@@ -20,11 +20,39 @@ void ATankPlayerController::BeginPlay()
 void ATankPlayerController::Tick( float DeltaTime )
 {
 	Super::Tick( DeltaTime );
+	AimTowardsCrosshair();
 }
+
+
+
+
+
 
 void ATankPlayerController::AimTowardsCrosshair()
 {
+	if (!GetControlledTank()) { return; }
 
+	FVector HitLocation; //Out parameter
+	UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"),*HitLocation.ToString() );
+	//Get world location through crosshair by ray tracing
+	//If it hits the landscape
+		//Tell controlled tank to aim at this point
+	GetSightRayHitLocation(HitLocation);
+}
+
+bool ATankPlayerController::GetSightRayHitLocation(FVector &OutHitLocation) const
+{
+	// Find the crosshair position in pixel coordinates
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+
+	auto ScreenLocation = FVector2D(ViewportSizeX * CrossHairXLocation, ViewportSizeY * CrossHairYLocation);
+
+	//UE_LOG(LogTemp, Warning, TEXT("ScreenLocation: %s"), *ScreenLocation.ToString() );
+
+	// "de-project" the screen position of the crosshair to a world direction
+	// Linetrace through along that look direction and see what was hit (up to max range)
+	return true;
 }
 
 
--- TankWars/Source/TankWars/Public/TankPlayerController.h
+++ TankWars/Source/TankWars/Public/TankPlayerController.h
@@ -16,8 +16,21 @@ class TANKWARS_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 	
 public:
-	ATank* GetControlledTank() const;
-	
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTime) override;
+
+
+private:
+	ATank* GetControlledTank() const;
+	
+	
+
+	void AimTowardsCrosshair(); //Moves tank barrel so that a shot would hit where the crosshair aims at.
+	bool GetSightRayHitLocation(FVector &OutHitLocation) const;  //Return an OUT parameter, true if hit landscape
+
+	UPROPERTY( EditAnywhere )
+	float CrossHairXLocation = 0.5f;
+
+	UPROPERTY( EditAnywhere )
+	float CrossHairYLocation = 0.3333f;
 };
