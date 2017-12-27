5c9486aaaa7760761c75677d02f1d5462a9bb7af
aleung124 <anthonyleung124@gmail.com>
2017-12-27 11:27:11 -0500

    AimingComponentClass initiated; BlueprintCallable function for getting the barrel pointer reference implemented

--- TankWars/Source/TankWars/Private/Tank.cpp
+++ TankWars/Source/TankWars/Private/Tank.cpp
@@ -9,6 +9,8 @@ ATank::ATank()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	//No need to protect pointers as added at construction
+	TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
 }
 
 // Called when the game starts or when spawned
@@ -25,6 +27,11 @@ void ATank::Tick(float DeltaTime)
 
 }
 
+void ATank::SetBarrelReference(UStaticMeshComponent* BarrelToSet)
+{
+	TankAimingComponent->SetBarrelReference(BarrelToSet);
+}
+
 // Called to bind functionality to input
 void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
@@ -35,6 +42,5 @@ void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 
 void ATank::AimAt(FVector HitLocation) 
 {
-	auto CurrentTankName = GetName();
-	UE_LOG(LogTemp, Warning, TEXT("%s aiming at %s"), *CurrentTankName, *HitLocation.ToString() );
+	TankAimingComponent->AimAt(HitLocation);
 }
--- TankWars/Source/TankWars/Public/Tank.h
+++ TankWars/Source/TankWars/Public/Tank.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "TankAimingComponent.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
 #include "Tank.generated.h"
@@ -17,11 +18,16 @@ public:
 
 	void AimAt(FVector HitLocation);
 
+	UFUNCTION(BlueprintCallable, Category = Setup)
+	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
+
 protected:
+	UTankAimingComponent * TankAimingComponent = nullptr;
+
+private:	
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
