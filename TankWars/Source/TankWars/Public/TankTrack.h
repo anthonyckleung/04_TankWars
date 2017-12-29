bfa2f03596613df1ceccbf8b40951029d886defe
aleung124 <anthonyleung124@gmail.com>
2017-12-29 10:37:39 -0500

    AddForceAtLocation() on Tank tracks implemented; need to resolve friction issue

--- TankWars/Source/TankWars/Public/TankTrack.h
+++ TankWars/Source/TankWars/Public/TankTrack.h
@@ -18,6 +18,10 @@ public:
 	// Sets throttle between -1 and +1
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void SetThrottle(float Throttle);
+
+	// Max force per track, in Newtons
+	UPROPERTY(EditDefaultsOnly)
+	float TrackMaxDrivingForce = 400000; //Assume 40 tonne tank, 1g acceleration
 	
 	
 };
