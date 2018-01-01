1815d5d102ad6a6de0d7ff854bf9d41772192bd9
aleung124 <anthonyleung124@gmail.com>
2018-01-01 17:33:37 -0500

    Linear Damping added; OnHit Collision event added on cpp

--- TankWars/Source/TankWars/Private/TankTrack.cpp
+++ TankWars/Source/TankWars/Private/TankTrack.cpp
@@ -4,29 +4,48 @@
 
 UTankTrack::UTankTrack()
 {
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
+
+}
+
+void UTankTrack::BeginPlay()
+{
+	OnComponentHit.AddDynamic(this, &UTankTrack::OnHit);
+}
+
+void UTankTrack::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit)
+{
+	DriveTrack();
+	ApplySideWaysForce();
+	CurrentThrottle = 0; //Reset Throttle to 0 to prevent continuous rolling
 }
 
 
-void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+
+void UTankTrack::ApplySideWaysForce()
 {
 	// Calculate Slippage Speed
 	auto SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
+	auto DeltaTime = GetWorld()->GetTimeSeconds();
 	auto CorrectionAcceleration = -(SlippageSpeed / DeltaTime) * GetRightVector();
 
 	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
-	auto CorrectionForce = (TankRoot->GetMass() * CorrectionAcceleration)/2; //divide by 2 because there are 2 tracks
+	auto CorrectionForce = (TankRoot->GetMass() * CorrectionAcceleration) / 2; //divide by 2 because there are 2 tracks
 	TankRoot->AddForce(CorrectionForce);
-
 }
 
 void UTankTrack::SetThrottle(float Throttle)
+{
+	CurrentThrottle = FMath::Clamp<float>(CurrentThrottle + Throttle,-1, 1);
+}
+
+void UTankTrack::DriveTrack()
 {
 	auto Name = GetName();
 	//UE_LOG(LogTemp, Warning, TEXT("%s throttle: %f"), *Name, Throttle);
 
 	//TODO: clamp actual throttle value so player can't over-drive
-	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	auto ForceApplied = GetForwardVector() * CurrentThrottle * TrackMaxDrivingForce;
 	auto ForceLocation = GetComponentLocation();
 	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
 	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
--- TankWars/Source/TankWars/Public/TankTrack.h
+++ TankWars/Source/TankWars/Public/TankTrack.h
@@ -15,9 +15,13 @@ class TANKWARS_API UTankTrack : public UStaticMeshComponent
 	GENERATED_BODY()
 	
 public:
+
+	
+
 	// Sets throttle between -1 and +1
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void SetThrottle(float Throttle);
+	
 
 	// Max force per track, in Newtons
 	UPROPERTY(EditDefaultsOnly)
@@ -25,7 +29,15 @@ public:
 	
 private:
 	UTankTrack();
+	
+	virtual void BeginPlay() override;
+	
+	float CurrentThrottle = 0;
+	void DriveTrack();
+	
+	void ApplySideWaysForce();
 
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction * ThisTickFunction) override;
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
 	
 };
